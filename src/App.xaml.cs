78363e5c795efc46fc701f69bb81f71d80895a1e
btigi <btigi@users.noreply.github.com>
2025-11-08 11:41:22 +0000

    Allow the hotkey to be customised

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -88,19 +88,23 @@ namespace rds
             if (_globalHotkey != null)
                 return;
 
-            const uint VK_BACK = 0x08;
             const uint VK_RETURN = 0x0D;
             const int HOTKEY_ID_SHOW = 9000;
             const int HOTKEY_ID_TOGGLE = 9001;
-            const uint MOD_CONTROL_ALT = 0x0002 | 0x0001;
             const uint MOD_ALT = 0x0001;
             
             try
             {
+                var modifiers = _configuration!.GetSection("Hotkeys:ShowWindow:Modifiers").Get<string[]>() ?? new[] { "Ctrl", "Alt" };
+                var keyName = _configuration["Hotkeys:ShowWindow:Key"] ?? "Backspace";
+                
+                var virtualKey = ParseVirtualKey(keyName);
+                var modifierFlags = ParseModifiers(modifiers);
+                
                 _globalHotkey = new GlobalHotkey(
                     MainWindow!,
                     HOTKEY_ID_SHOW,
-                    VK_BACK,
+                    virtualKey,
                     () =>
                     {
                         Dispatcher.Invoke(() =>
@@ -118,7 +122,7 @@ namespace rds
                             }
                         });
                     },
-                    MOD_CONTROL_ALT);
+                    modifierFlags);
 
                 _toggleMaximizeHotkey = new GlobalHotkey(
                     MainWindow!,
@@ -152,6 +156,105 @@ namespace rds
             {
                 System.Diagnostics.Debug.WriteLine($"Failed to register global hotkey: {ex.Message}");
             }
+        }
+
+        private uint ParseVirtualKey(string keyName)
+        {
+            return keyName.ToUpperInvariant() switch
+            {
+                "BACKSPACE" => 0x08,
+                "TAB" => 0x09,
+                "ENTER" => 0x0D,
+                "SHIFT" => 0x10,
+                "CTRL" => 0x11,
+                "ALT" => 0x12,
+                "PAUSE" => 0x13,
+                "CAPSLOCK" => 0x14,
+                "ESC" => 0x1B,
+                "ESCAPE" => 0x1B,
+                "SPACE" => 0x20,
+                "PAGEUP" => 0x21,
+                "PAGEDOWN" => 0x22,
+                "END" => 0x23,
+                "HOME" => 0x24,
+                "LEFT" => 0x25,
+                "UP" => 0x26,
+                "RIGHT" => 0x27,
+                "DOWN" => 0x28,
+                "INSERT" => 0x2D,
+                "DELETE" => 0x2E,
+                "0" => 0x30,
+                "1" => 0x31,
+                "2" => 0x32,
+                "3" => 0x33,
+                "4" => 0x34,
+                "5" => 0x35,
+                "6" => 0x36,
+                "7" => 0x37,
+                "8" => 0x38,
+                "9" => 0x39,
+                "A" => 0x41,
+                "B" => 0x42,
+                "C" => 0x43,
+                "D" => 0x44,
+                "E" => 0x45,
+                "F" => 0x46,
+                "G" => 0x47,
+                "H" => 0x48,
+                "I" => 0x49,
+                "J" => 0x4A,
+                "K" => 0x4B,
+                "L" => 0x4C,
+                "M" => 0x4D,
+                "N" => 0x4E,
+                "O" => 0x4F,
+                "P" => 0x50,
+                "Q" => 0x51,
+                "R" => 0x52,
+                "S" => 0x53,
+                "T" => 0x54,
+                "U" => 0x55,
+                "V" => 0x56,
+                "W" => 0x57,
+                "X" => 0x58,
+                "Y" => 0x59,
+                "Z" => 0x5A,
+                "F1" => 0x70,
+                "F2" => 0x71,
+                "F3" => 0x72,
+                "F4" => 0x73,
+                "F5" => 0x74,
+                "F6" => 0x75,
+                "F7" => 0x76,
+                "F8" => 0x77,
+                "F9" => 0x78,
+                "F10" => 0x79,
+                "F11" => 0x7A,
+                "F12" => 0x7B,
+                _ => 0x08
+            };
+        }
+
+        private uint ParseModifiers(string[] modifiers)
+        {
+            uint flags = 0;
+            foreach (var modifier in modifiers)
+            {
+                var mod = modifier.ToUpperInvariant();
+                if (mod == "CTRL" || mod == "CONTROL")
+                {
+                    flags |= 0x0002;
+                }
+                else if (mod == "ALT")
+                {
+                    flags |= 0x0001;
+                }
+                else if (mod == "SHIFT")
+                {
+                    flags |= 0x0004;
+                }
+            }
+            return flags;
         }
 
         private void ShowMenuItem_Click(object sender, RoutedEventArgs e)
